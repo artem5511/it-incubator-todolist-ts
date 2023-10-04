2f0a1e42bfd919a7ee7858ba3d00e124f2f13480
artem_ <artem_5511@mail.ru>
2023-10-04 23:36:03 +0300

    add reduser

--- src/state/user-reducer.test.ts
+++ src/state/user-reducer.test.ts
@@ -2,7 +2,6 @@ import { userReducer } from '../state/user-reduser'
 
 test('user reducer should increment only age', () => {
     const startState = {age: 20, childrenCount: 2, name: 'Dimych'}
-
     const endState = userReducer(startState, {type: 'INCREMENT-AGE'})
 
     expect(endState.age).toBe(21)
@@ -12,4 +11,8 @@ test('user reducer should increment only age', () => {
 test('user reducer should increment only childrenCount', () => {
     const startState = {age: 20, childrenCount: 2, name: 'Dimych'}
     // your code here
+    const newName = 'Voktor'
+    const endState = userReducer(startState, {type: 'CHANGE-NAME', newName: newName})
+
+    expect(endState.name).toBe(newName)
 })
